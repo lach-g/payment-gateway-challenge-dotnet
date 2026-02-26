3a6689e7f28a99ea903fc9dd1478a71056dc02be
Lachlan Greenland <59797179+lach-g@users.noreply.github.com>
2026-02-26 16:32:47 +0800

    feat: DTOs
    * feat: vscode launch config
    
    * feat: DTOs. Rework payment repository. Integration tests

---
+++ src/PaymentGateway.Api/Models/Responses/ErrorResponse.cs
@@ -0,0 +1,9 @@
+namespace PaymentGateway.Api.Models.Responses;
+
+/// <summary>
+/// Merchant-facing response after processing a card payment that resulted in an error.
+/// </summary>
+public class ErrorResponse
+{
+    public required string Message { get; set; }
+}
---
+++ test/PaymentGateway.Api.Tests.Integration/PaymentsControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+using PaymentGateway.Api.Controllers;
+using PaymentGateway.Api.Models;
+using PaymentGateway.Api.Models.Responses;
+using PaymentGateway.Api.Services;
+
+namespace PaymentGateway.Api.Tests.Integration;
+
+public class PaymentsControllerTests
+{
+    private readonly Random _random = new();
+    private readonly HttpClient _client;
+    private readonly IPaymentsRepository _paymentsRepository;
+
+    public PaymentsControllerTests()
+    {
+        _paymentsRepository = new PaymentsRepository();
+        var webApplicationFactory = new WebApplicationFactory<PaymentsController>();
+        _client = webApplicationFactory.WithWebHostBuilder(builder =>
+            builder.ConfigureServices(services => ((ServiceCollection)services)
+                .AddSingleton(_paymentsRepository)))
+            .CreateClient();
+    }
+    
+    [Fact]
+    public async Task GetPaymentAsync_RequestStoredPaymentId_RespondsWithPayment()
+    {
+        // Arrange
+        var payment = new PostPaymentResponse
+        {
+            Id = Guid.NewGuid(),
+            Status = PaymentStatus.Authorized,
+            ExpiryYear = _random.Next(DateTime.Now.Year + 1, DateTime.Now.Year + 10),
+            ExpiryMonth = _random.Next(1, 12),
+            Amount = _random.Next(1, 10000),
+            CardNumberLastFour = _random.Next(1111, 9999),
+            Currency = "AUD"
+        };
+        _paymentsRepository.Add(payment);
+
+        // Act
+        var response = await _client.GetAsync($"/api/Payments/{payment.Id}");
+        var paymentResponse = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(paymentResponse);
+        Assert.Equal(payment.Id, paymentResponse.Id);
+        Assert.Equal(payment.Status, paymentResponse.Status);
+        Assert.Equal(payment.ExpiryYear, paymentResponse.ExpiryYear);
+        Assert.Equal(payment.ExpiryMonth, paymentResponse.ExpiryMonth);
+        Assert.Equal(payment.Amount, paymentResponse.Amount);
+        Assert.Equal(payment.CardNumberLastFour, paymentResponse.CardNumberLastFour);
+        Assert.Equal(payment.Currency, paymentResponse.Currency);
+    }
+
+    [Fact]
+    public async Task GetPaymentAsync_RequestNonExistentPaymentId_RespondsNotFound()
+    {
+        // Act
+        var response = await _client.GetAsync($"/api/Payments/{Guid.NewGuid()}");
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetPaymentAsync_RequestEmptyGuid_RespondsWithBadRequest()
+    {
+        // Act
+        var response = await _client.GetAsync($"/api/Payments/{Guid.Empty}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}

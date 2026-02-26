1801471c08119b8f12179f9f8f55309f67f42fcc
Lachlan Greenland <59797179+lach-g@users.noreply.github.com>
2026-02-26 17:18:07 +0800

    feat: logging api versioning

--- src/PaymentGateway.Api/Controllers/PaymentsController.cs
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -1,18 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asp.Versioning;
+
+using Microsoft.AspNetCore.Mvc;
 
 using PaymentGateway.Api.Models.Responses;
 using PaymentGateway.Api.Services;
 
 namespace PaymentGateway.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
 [ApiController]
 public class PaymentsController : Controller
 {
+    private readonly ILogger<PaymentsController> _logger;
     private readonly IPaymentsRepository _paymentsRepository;
 
-    public PaymentsController(IPaymentsRepository paymentsRepository)
+    public PaymentsController(ILogger<PaymentsController> logger, IPaymentsRepository paymentsRepository)
     {
+        ArgumentNullException.ThrowIfNull(logger);
+        ArgumentNullException.ThrowIfNull(paymentsRepository);
+
+        _logger = logger;
         _paymentsRepository = paymentsRepository;
     }
 
@@ -21,13 +29,17 @@ public class PaymentsController : Controller
     {
         if (id == Guid.Empty) 
         {
+            _logger.LogWarning("Received request for payment with empty GUID.");
             return BadRequest(new ErrorResponse { Message = "Payment ID cannot be empty." });
         }
 
         if (!_paymentsRepository.TryGet(id, out var payment))
         {
+            _logger.LogWarning("Received request for non-existent payment with ID {Id}.", id);
             return NotFound(new ErrorResponse { Message = $"Payment with ID {id} not found." });
         }
+
+        _logger.LogInformation("Responding with payment for ID {Id} successfully.", id);
 
         return new OkObjectResult(payment);
     }
